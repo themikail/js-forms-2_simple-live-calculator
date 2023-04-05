f3e8d1ba92cf6acae9129bbfdb9f8ca29af9cb83
themikail <hello@mikailgokce.com>
2023-04-05 15:56:02 +0200

    finish exercise

--- js/index.js
+++ js/index.js
@@ -3,3 +3,13 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
+
+firstInput.addEventListener("input", function () {
+  const firstValue = Number(firstInput.value) * Number(secondInput.value);
+  result.textContent = firstValue;
+});
+
+secondInput.addEventListener("input", function () {
+  const secondValue = Number(firstInput.value) * Number(secondInput.value);
+  result.textContent = secondValue;
+});
